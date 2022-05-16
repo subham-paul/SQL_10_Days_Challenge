e85ee3c3aae057a6487e65d945e2210e6c0a0e39
Subham Paul <subhampaul456@gmail.com>
2022-05-16 22:40:27 +0530

    DAY 1 - SELECT

--- Day-1_Select/183_Customers_Who_Never_Order.sql
+++ Day-1_Select/183_Customers_Who_Never_Order.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 SELECT Customers.name AS customers FROM Customers
 WHERE Customers.id NOT IN
-    (
-        SELECT customerID FROM Orders
-    );
+(
+    SELECT customerID FROM Orders
+);
