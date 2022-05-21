157c326355e75a9ac0696e1c610919b8ca55451d
Subham Paul <subhampaul456@gmail.com>
2022-05-21 22:27:56 +0530

    Day 5 SQL Challenge

---
+++ Day-5_Union/175_Combine_Two_Tables.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+
+SELECT p.firstName, p.lastName, a.city, a.state FROM Person p    #accessing first hench LEFT
+LEFT JOIN Address a USING (personId)    # accessing second hence RIGHT. but we want every row in person so LEFT JOIN.
