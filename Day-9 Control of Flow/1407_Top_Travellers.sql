9ee67068622a05f41f5ff534c03f61209e577c09
Subham <subhampaul456@gmail.com>
2022-06-06 08:43:08 +0530

    1407 top travaller problem

--- Day-9 Control of Flow/1407_Top_Travellers.sql
+++ Day-9 Control of Flow/1407_Top_Travellers.sql
@@ -1,10 +1,9 @@
 # Write your MySQL query statement below
 
-select 
-	name,
-	sum(if(distance is not null, distance, 0)) as travelled_distance
-from users u 
-left join rides r 
-	on u.id = r.user_id 
-group by name
-order by 2 desc, 1 
+select u.name, ifnull(sum(r.distance),0) as travelled_distance 
+from users u
+left join
+rides r
+on u.id = r.user_id
+group by u.id
+order by travelled_distance desc, u.name
